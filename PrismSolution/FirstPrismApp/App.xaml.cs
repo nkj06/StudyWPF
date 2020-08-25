dbb54472e767fb56ce30bce45cb0ca991563843a
nkj06 <nkj960425@gmail.com>
2020-08-25 09:15:47 +0900

    띠용

---
+++ PrismSolution/FirstPrismApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using FirstPrismApp.Views;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FirstPrismApp
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ PrismSolution/FirstPrismApp/Views/MainView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Ioc;
+using Prism.Regions;
+using System.Windows;
+
+namespace FirstPrismApp.Views
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainView : Window
+    {
+        IContainerExtension _container;
+        IRegionManager _regionManager;
+
+        public MainView(IContainerExtension container, IRegionManager regionManager)
+        {
+            InitializeComponent();
+            // regionManager.RegisterViewWithRegion("ContentRegion", typeof(SubView));
+
+            _container = container;
+            _regionManager = regionManager;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var view = _container.Resolve<SubView>();
+            var region = _regionManager.Regions["ContentRegion"];
+            region.Add(view);
+        }
+    }
+}
